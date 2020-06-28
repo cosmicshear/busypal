d7e0fe9bfd81bbdfd288812de74db51f1fb33fde
Erfan Nourbakhsh <erfanxyz@gmail.com>
2020-06-28 07:30:20 -0700

    Add source

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='busypal',
+      version='1.0',
+      description='Shows a busy indicator for long-running operations while it is still processing',
+      url='https://github.com/cosmicshear/busypal',
+      author='Erfan Nourbakhsh',
+      author_email='erfanxyz@gmail.com',
+      license='MIT',
+      packages=['busypal'],
+      install_requires=[
+          "colored",
+      ],
+      zip_safe=False)
